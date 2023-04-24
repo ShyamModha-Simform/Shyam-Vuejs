7df8bc73e27a3284b52b4be353f25d8dcd597e54
ShyamModha-Simform <shyam.m@simformsolutions.com>
2023-04-24 12:14:37 +0530

    Added: Moved ShowPrice method from child to Parent

--- src/Components/GalleryCardList.vue
+++ src/Components/GalleryCardList.vue
@@ -1,6 +1,13 @@
+<template>
+  <div class="container">
+      <GalleryCard :carDetail="car" v-for="car in carDetails" :key="car.carName" @showPrice="displayPrice"/>
+  </div>
+</template>
+
 <script>
 import GalleryCard from './GalleryCard.vue'
 import carDetails from '../assests/car-details.json'
+import swal from 'sweetalert'
 
 export default {
   name: 'GalleryCardList',
@@ -11,12 +18,12 @@ export default {
   },
   components: {
     GalleryCard
-  }
+  },
+  methods: {
+    displayPrice(carPrice){
+      swal("Unbeatable prices - indulge in your dream car today!",` For only $${carPrice}/-`);
+    }
+  },
 }
 </script>
 
-<template>
-  <div class="container">
-      <GalleryCard :carDetail="car" v-for="car in carDetails" :key="car.carName"/>
-  </div>
-</template>
