9a98008fc300f0127133fe3ce69e6c12e24128c8
ShyamModha-Simform <shyam.m@simformsolutions.com>
2023-05-10 17:14:15 +0530

    Fix: Responsive ness issue

--- src/views/CarDetailsView.vue
+++ src/views/CarDetailsView.vue
@@ -1,23 +1,35 @@
 <template>
-  
-  <div class="car-card-container">
+  <div class="car-card-container mx-4">
     <div>
-    <RouterLink :to="{name:'home'}">
-      <BaseButton class="card" size="lg">back</BaseButton>
-    </RouterLink>
-  </div>
-  <div class="car-detail--container">
+      <RouterLink :to="{ name: 'home' }">
+        <BaseButton class="card" size="lg">back</BaseButton>
+      </RouterLink>
+    </div>
+    <!-- <div class="car-detail--container flex-wrap">
+      <div class="container--left w-50">
+        <h1>{{ carDetail.name }}</h1>
+        <h4>Price: {{ carDetail.price }}</h4>
+        <p>{{ carDetail.details }}</p>
+      </div>
+      <div class="container--right w-50">
+        <img :src="carDetail.image" alt="Car Image" />
+      </div>
+    </div> -->
+    <div class="card mb-3 mx-sm-2" style="max-width: 70rem;">
+      <div class="row g-0">
+        <div class="col-md-7">
+          <img :src="carDetail.image" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-5">
+          <div class="card-body">
+            <h1 class="card-title">{{ carDetail.name }}</h1>
+            <h4 class="card-text">Price:-{{ carDetail.price }}</h4>
+            <p class="card-text">{{ carDetail.details }}</p>
+          </div>
+        </div>
 
-  
-    <div class="container--left w-50">
-      <h1>{{ carDetail.name }}</h1>
-      <h4>Price: {{ carDetail.price }}</h4>
-      <p>{{ carDetail.details }}</p>
+      </div>
     </div>
-    <div class="container--right w-50">
-      <img :src="carDetail.image" alt="Car Image" />
-    </div>
-  </div>
   </div>
 </template>
 
@@ -51,33 +63,55 @@ h4,
 p {
   color: #606d75;
 }
-h1{
-    margin-block: 10px; 
-}
-p, h4{
-    margin-block: 20px;
-}
-img{
-    width: 100%;
-}
-.car-card-container{
-  display: flex;
-  flex-direction: column;
-    width: min(100%, 70rem);
-    margin-inline: auto;
-    margin-top: 10%;
+
+h1 {
+  margin-block: 10px;
 }
 
-.car-card-container > button {
+p,
+h4 {
+  margin-block: 20px;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.car-card-container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 70rem;
+  max-width: fit-content;
+  margin-inline: auto;
+  margin-top: 10%;
+}
+
+.car-card-container>button {
   margin-right: 10px;
 }
+
 .car-detail--container {
   display: flex;
   background-color: whitesmoke;
   padding: 1.5em 1.5em 1.5em 1.5em;
   box-sizing: border-box;
+  margin-inline: 10px;
   border-radius: 10px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: transform 0.2s ease-in-out;
+}
+
+.container--right,
+.container--left {
+  min-width: 300px;
+}
+
+@media screen and (max-width:680px) {
+  .car-card-container {
+    width: fit-content;
+  }
 }
 </style>
